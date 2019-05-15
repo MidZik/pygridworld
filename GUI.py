d8fcdc051a31c4d9260d081a0a879eaf49612d84
MidZik <matt.idzik1@gmail.com>
2019-05-14 20:36:05 -0500

    Added second plot that tracks notable populations. Reduced how often plots are redrawn.

--- GUI.py
+++ GUI.py
@@ -46,7 +46,7 @@ class PositionEntityObject:
 
 class WorldWindow(pyglet.window.Window):
     def __init__(self, em: ECS.EntityManager):
-        super().__init__(resizable=True)
+        super().__init__(resizable=True, width=800, height=800)
         self.em = em
         #em.sig_update_complete.connect(self.on_em_update_complete)
         self.update_from_em()
@@ -81,19 +81,26 @@ class WorldWindow(pyglet.window.Window):
                 del _entity_object_map[eid]
 
 class _Plotter:
-    def __init__(self):
+    def __init__(self, em: ECS.EntityManager):
         plt.ion()
+        self.em = em
         self.fig = plt.figure()
         self.majname_data = {}
+        self.pop_plots = {}
         self.tick_data = [0]
         self.winner_mean_score_data = [0]
         self.all_mean_score_data = [0]
+        
+        plt.subplot(211)
         self.winners_mean_score_plot, = plt.plot(self.tick_data, self.winner_mean_score_data, label='winners')
         self.all_mean_score_plot, = plt.plot(self.tick_data, self.all_mean_score_data, label='all')
-        
         plt.xlabel('Tick')
         plt.ylabel('Score')
         plt.title('Judge Stats')
+        
+        self.log_pop_counts(em)
+        self.plot_pop_counts()
+        
         plt.show()
     
     def on_event(self, event_data):
@@ -113,11 +120,14 @@ class _Plotter:
             winner_mean = winner_total / len(winners)
             total_mean = total / len(entity_scores)
             
-            self.tick_data.append(test_em.tick)
+            self.tick_data.append(self.em.tick)
             self.winner_mean_score_data.append(winner_mean)
             self.all_mean_score_data.append(total_mean)
             
-            if test_em.tick % 10000 == 0:
+            self.log_pop_counts(self.em)
+            
+            if self.em.tick % 50000 == 0:
+                plt.subplot(211)
                 self.winners_mean_score_plot.set_data(self.tick_data, self.winner_mean_score_data)
                 self.all_mean_score_plot.set_data(self.tick_data, self.all_mean_score_data)
                 
@@ -125,14 +135,53 @@ class _Plotter:
                 ax.relim()
                 ax.autoscale_view()
                 
+                self.plot_pop_counts()
+                
                 self.fig.canvas.draw()
                 self.fig.canvas.flush_events()
+    
+    def log_pop_counts(self, em: ECS.EntityManager):
+        name_map = em.component_maps[com.Name]
+        
+        name: com.Name
+        for eid, name in name_map.items():
+            maj = name.major_name
+            
+            try:
+                x,y = self.majname_data[maj]
+            except LookupError:
+                x = []
+                y = []
+                self.majname_data[maj] = (x, y)
+            
+            if not x or x[-1] != em.tick:
+                x.append(em.tick)
+                y.append(0)
+            
+            y[-1] += 1
+    
+    def plot_pop_counts(self):
+        plt.subplot(212)
+        for maj, (x, y) in self.majname_data.items():
+            # only plot populations that have lived a non-trivial amount of time
+            if len(x) >= 10:
+                try:
+                    plot = self.pop_plots[maj]
+                except LookupError:
+                    plot, = plt.plot(x, y)
+                    self.pop_plots[maj] = plot
+                else:
+                    plot.set_data(x, y)
+        
+        ax = plt.gca()
+        ax.relim()
+        ax.autoscale_view()
 
 if __name__ == '__main__':
     test_em = core.setup_test_em()
     
     # some debug plotting stuff
-    plotter = _Plotter()
+    plotter = _Plotter(test_em)
     
     events: com.SEvents = test_em.scomponents_map[com.SEvents]
     events._sig_event.connect(plotter.on_event)
