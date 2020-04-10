46ee2bc756904fcecf27a8af1c2b5f85be290cb3
MidZik <matt.idzik1@gmail.com>
2020-04-10 18:18:01 -0500

    Added state serialization to file on event

--- SimulationManager.py
+++ SimulationManager.py
@@ -4,12 +4,16 @@
 from importlib import util
 from multiprocessing.connection import Connection
 from multiprocessing import Pipe, Process
-from threading import Lock
+from threading import Lock, Thread
+from pathlib import Path
 import json
+import os
+from datetime import datetime
+from queue import Queue, Empty
 
 
 class SimulationRunner:
-    def __init__(self, simulation_folder_path):
+    def __init__(self, simulation_folder_path, runner_working_dir):
         simulation_module_path = (simulation_folder_path / "simulation.pyd").resolve(True)
         spec = util.spec_from_file_location("simulation", simulation_module_path)
         self.module = util.module_from_spec(spec)
@@ -17,6 +21,24 @@ class SimulationRunner:
 
         self.simulation = self.module.Simulation()
         self.simulation.set_event_callback(self._on_simulation_event)
+
+        self._working_dir = Path(runner_working_dir).resolve()
+        self._saved_states_dir = (self._working_dir / 'saved_states').resolve()
+        self._saved_states_dir.mkdir(exist_ok=True)
+        self._logs_dir = (self._working_dir / 'logs').resolve()
+        self._logs_dir.mkdir(exist_ok=True)
+        self._cur_log_file = (self._logs_dir / f'log-{datetime.now():%Y-%m-%d}.txt').resolve()
+
+        self._state_jsons_to_save_queue = Queue(100)
+        self._state_writer_thread = Thread(target=self._queued_states_writer)
+        self._state_writer_thread.start()
+
+    def cleanup(self):
+        # Shut off and join all threads
+        self.simulation.stop_simulation()
+        self._state_jsons_to_save_queue.put(None)
+        self._state_jsons_to_save_queue.join()
+        self._state_writer_thread.join()
 
     def start_simulation(self):
         self.simulation.start_simulation()
@@ -44,12 +66,37 @@ class SimulationRunner:
 
     def _on_simulation_event(self, events_json):
         events_obj = json.loads(events_json)
-        state_json = self.get_state_json()
-        state_obj = json.loads(state_json)
+        self._state_jsons_to_save_queue.put(self.get_state_json())
+
+    def _queued_states_writer(self):
+        while True:
+            try:
+                state_json_to_write = self._state_jsons_to_save_queue.get(True, 1.0)
+            except Empty:
+                continue
+
+            try:
+                if state_json_to_write is None:
+                    break  # This is the sole exit condition for this loop
+                else:
+                    state_obj = json.loads(state_json_to_write)
+                    tick = state_obj['singletons']['STickCounter']
+                    if isinstance(tick, int):
+                        state_file_path = (self._saved_states_dir / f'{tick}.json').resolve()
+                        with state_file_path.open('w') as state_file:
+                            state_file.write(state_json_to_write)
+                    else:
+                        with self._cur_log_file.open('a') as log:
+                            log.write(f'[{datetime.now():%H:%M:%S}] Could not write state, bad tick format: {tick}\n')
+            except Exception as e:
+                with self._cur_log_file.open('a') as log:
+                    log.write(f'[{datetime.now():%H:%M:%S}] Unknown exception when writing state: {e}\n')
+            finally:
+                self._state_jsons_to_save_queue.task_done()
 
 
-def simulation_runner_loop(con: Connection, simulation_folder_path):
-    runner = SimulationRunner(simulation_folder_path)
+def simulation_runner_loop(con: Connection, simulation_folder_path, runner_working_dir):
+    runner = SimulationRunner(simulation_folder_path, runner_working_dir)
 
     while True:
         try:
@@ -90,11 +137,13 @@ def simulation_runner_loop(con: Connection, simulation_folder_path):
             # connection closed, end the simulation
             break
 
+    runner.cleanup()
+
 
 class SimulationRunnerProcess:
-    def __init__(self, simulation_folder_path):
+    def __init__(self, simulation_folder_path, runner_working_dir):
         self._conn, child_conn = Pipe()
-        self._process = Process(target=simulation_runner_loop, args=(child_conn, simulation_folder_path), daemon=True)
+        self._process = Process(target=simulation_runner_loop, args=(child_conn, simulation_folder_path, runner_working_dir), daemon=True)
         self._lock = Lock()
 
     def start_process(self):
