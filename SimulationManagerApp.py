8bbe48dbf77e835ad70de4de0084b597aa4fe090
MidZik <matt.idzik1@gmail.com>
2020-05-10 04:39:35 -0500

    Added timeline navigation and point data display

--- SimulationManagerApp.py
+++ SimulationManagerApp.py
@@ -2,6 +2,38 @@ import SimulationManager as sm
 import window
 from typing import Optional
 from PyQt5 import QtCore, QtGui, QtWidgets
+from pathlib import Path
+
+
+class _TaskRunner(QtCore.QRunnable):
+    def __init__(self, task):
+        super().__init__()
+        self._task = task
+
+    @QtCore.pyqtSlot()
+    def run(self) -> None:
+        self._task.do_work()
+
+
+class _Task(QtCore.QObject):
+    def do_work(self):
+        pass
+
+    def runner(self):
+        return _TaskRunner(self)
+
+
+class _ReadFileTask(_Task):
+    read_done = QtCore.pyqtSignal(str)
+
+    def __init__(self, file_path):
+        super().__init__()
+        self.file_path = Path(file_path).resolve()
+
+    def do_work(self):
+        with self.file_path.open('r') as f:
+            text = f.read()
+            self.read_done.emit(text)
 
 
 class App:
@@ -18,9 +50,12 @@ class App:
         ui.actionOpen_Project.triggered.connect(self._open_project)
 
         ui.timelinePointTree.currentItemChanged.connect(self._on_timeline_point_tree_current_item_changed)
+        ui.timelinePointTree.itemActivated.connect(self._on_timeline_point_tree_item_activated)
 
         self._project: Optional[sm.TimelinesProject] = None
         self._current_timeline: Optional[sm.Timeline] = None
+
+        self._thread_pool = QtCore.QThreadPool()
 
     def _open_project(self):
         from PyQt5.QtWidgets import QFileDialog
@@ -30,15 +65,35 @@ class App:
         self.set_current_timeline(None)
 
     def _on_timeline_point_tree_current_item_changed(self, current, previous):
+        if current is None:
+            self._ui.stateJsonTextEdit.setPlainText("")
+            return
+
         point = current.data(0, App._PointRole)
         timeline = current.data(0, App._TimelineRole)
 
+        def update_text(text):
+            if self._ui.timelinePointTree.currentItem() == current:
+                self._ui.stateJsonTextEdit.setPlainText(text)
+
         if point is not None:
-            self._ui.stateJsonTextEdit.setPlainText(f"point {point}")
+            point_file_path = point.get_file_path()
+            if point_file_path is not None:
+                task = _ReadFileTask(point_file_path)
+                self._ui.stateJsonTextEdit.setPlainText(f"loading point {point}")
+                task.read_done.connect(update_text)
+                self._thread_pool.start(task.runner())
+            else:
+                self._ui.stateJsonTextEdit.setPlainText(f"Selected point has no data.")
         elif timeline is not None:
             self._ui.stateJsonTextEdit.setPlainText(f"timeline {timeline}")
         else:
             raise RuntimeWarning("Selected timeline tree item with no attached data.")
+
+    def _on_timeline_point_tree_item_activated(self, item, column):
+        timeline = item.data(0, App._TimelineRole)
+        if timeline is not None:
+            self.set_current_timeline(timeline.timeline_id)
 
     def set_current_timeline(self, timeline_id):
         self._current_timeline = self._project.get_timeline(timeline_id)
