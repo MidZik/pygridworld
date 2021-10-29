5fa8f317b3647f1924f7dc5287293ec480c7924a
MidZik <matt.idzik1@gmail.com>
2021-10-29 05:33:34 -0500

    Copied Signal from gwsignal to _utils
    Preparing for gwsignal to be removed.

--- simma/_utils.py
+++ simma/_utils.py
@@ -3,6 +3,9 @@ import json
 from pathlib import Path
 import os
 from contextlib import asynccontextmanager, contextmanager
+from inspect import ismethod
+import weakref
+from typing import Optional, Callable, TypeVar
 
 
 T = TypeVar('T')
@@ -107,3 +110,92 @@ class RWLock:
     async def reading_upgradable(self):
         async with self._write_lock, self.reading():
             yield self._make_upgrader()
+
+
+class Signal:
+    """
+    A signal is an observable object. Callables can be connected to the signal,
+    which will then be called whenever the signal is emitted.
+    The order that connected callbacks are called is undefined. It is only guaranteed that each connected
+    callback will be called exactly once for each time the signal is emitted.
+    """
+    __slots__ = ('_callbacks',)
+
+    def __init__(self):
+        self._callbacks: list[tuple[Optional[weakref.ref], Callable, tuple]] = []
+
+    def connect(self, callback, *binds):
+        """
+        Connects a callback to the signal. Whenever the signal is emitted, the callback will be called.
+        :param callback: callable to connect to the signal
+        :param binds: additional parameters that should be passed to the callback whenever the signal is emitted
+        """
+        if ismethod(callback):
+            # Bound method case
+            self_weakref = weakref.ref(callback.__self__)
+            self._callbacks.append((self_weakref, callback.__func__, binds))
+        else:
+            # Function/callable case
+            self._callbacks.append((None, callback, binds))
+
+    def disconnect(self, callback):
+        """
+        Disconnect a given callback from the signal.
+        The callback will no longer be called whenever the signal is emitted.
+        :param callback: Callable to disconnect.
+        """
+        i = self._find_callback(callback)
+        self._remove_index(i)
+
+    def emit(self, *args):
+        """
+        Emits the signal, calling all connected callbacks with the provided args.
+        :param args: Args to pass to each callback
+        """
+        for i in range(len(self._callbacks) - 1, -1, -1):
+            ref, callback, binds = self._callbacks[i]
+
+            if ref is not None:
+                # Bound method case
+                obj = ref()
+                if obj is not None:
+                    callback(obj, *args, *binds)
+                else:
+                    # slot object was deleted earlier, remove it.
+                    self._remove_index(i)
+            else:
+                # Function case
+                callback(*args, *binds)
+
+    def _find_callback(self, callback):
+        if ismethod(callback):
+            # Bound method case
+            expected_self = callback.__self__
+            expected_callback = callback.__func__
+        else:
+            # Function/callable case
+            expected_self = None
+            expected_callback = callback
+
+        for i, (ref, callback, binds) in enumerate(self._callbacks):
+            found_self = None
+            if ref is not None:
+                found_self = ref()
+                if found_self is None:
+                    continue
+
+            if found_self is expected_self and callback is expected_callback:
+                return i
+
+        raise ValueError("Unable to find a connection with the given slot.")
+
+    def _remove_index(self, index):
+        """
+        Removes an item from the callback list by putting the last item into its position. This makes this an O(1)
+        operation, at the expense of making callback calling order undefined.
+        :param index: Index to remove from the slots list.
+        """
+        if index == len(self._callbacks) - 1 or index == -1:
+            self._callbacks.pop()
+        else:
+            self._callbacks[index] = self._callbacks.pop()
