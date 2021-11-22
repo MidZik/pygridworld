6f60d3a273494d92ace4a2640444f57edde6d977
MidZik <matt.idzik1@gmail.com>
2021-11-21 22:37:00 -0600

    Changed simulation client to create its own channel

--- simma/simulation/client.py
+++ simma/simulation/client.py
@@ -31,8 +31,9 @@ class Client:
     def make_channel(address):
         return grpc.aio.insecure_channel(address)
 
-    def __init__(self, channel, token):
-        self._channel = channel
+    def __init__(self, address, token):
+        self._channel = Client.make_channel(address)
+        self.address = address
         self.token = token
 
     def _create_metadata(self, *items):
